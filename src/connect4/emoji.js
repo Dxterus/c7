c794e9cb41bb8fdd56ff848f363f3ed265b90c77
JP Eliares <Jeliares@paypal.com>
2018-05-10 10:08:09 -0700

    update

--- src/connect4/emoji.js
+++ src/connect4/emoji.js
@@ -9,7 +9,9 @@ module.exports = {
         6: ':six:',
         7: ':seven:',
         8: ':eight:',
-        9: ':nine:'
+        9: ':nine:',
+        10: ':ten',
+        11: ':eleven'
     },
     normal: {
         circle: {
